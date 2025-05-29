594e6b4f7ad21c4764dec7fb1a5caa70b3225784
Christopher Ernenwein <chris.ernenwein@gmail.com>
2025-05-29 11:16:28 -0400

    kk

--- install/bluetooth/auto_connect.sh
+++ install/bluetooth/auto_connect.sh
@@ -2,6 +2,8 @@
 DEVICE_NAME="JLab GO Air Sport"
 MAC=$(bluetoothctl devices | grep "$DEVICE_NAME" | awk '{print $2}')
 [ -z "$MAC" ] && echo "Device not found" && exit 1
+LOGFILE="/tmp/bluetooth_auto_connect.log"
+echo "$(date): Starting auto-connect" >> $LOGFILE
 bluetoothctl << EOF
 power on
 connect $MAC
