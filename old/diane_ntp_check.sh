6ff5d772800cf0473903921c185be97adf613a9f
Christopher Ernenwein <chris.ernenwein@gmail.com>
2025-05-30 10:46:33 -0400

    fix

--- old/diane_ntp_check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-status=$(timedatectl status)
-synced=$(echo "$status" | grep "System clock synchronized:" | awk '{print $4}')
-ntp_active=$(echo "$status" | grep "NTP service:" | awk '{print $3}')
-ntp_server=$(echo "$status" | grep "Server:" | awk '{print $2}')
-echo "[Diane Time Check]"
-echo "Clock synchronized: $synced"
-echo "NTP service active: $ntp_active"
-[ -n "$ntp_server" ] && echo "Current NTP server: $ntp_server"
--- old/diane_setup_log2ram.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-echo "[Diane Log2RAM Setup]"
-sudo apt update -qq
-sudo apt install -y git
-git clone https://github.com/azlux/log2ram.git /tmp/log2ram
-cd /tmp/log2ram
-sudo ./install.sh
-echo "Log2RAM installed. You may reboot to apply it fully."
