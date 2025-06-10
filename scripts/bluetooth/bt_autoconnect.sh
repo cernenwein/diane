873c470c375f0b9ef3fcd8523eb199df5f27a07c
chris <chris.ernenwein@gmail.com>
2025-06-10 14:26:57 -0400

    fix

--- scripts/bluetooth/bt_autoconnect.sh
+++ scripts/bluetooth/bt_autoconnect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TRUSTED_MACS=$(cat /home/diane/bluetooth_trusted_devices.txt)
+TRUSTED_MACS=$(cat /home/diane/diane/bluetooth_trusted_devices.txt)
 
 for MAC in $TRUSTED_MACS; do
   bluetoothctl connect "$MAC"
